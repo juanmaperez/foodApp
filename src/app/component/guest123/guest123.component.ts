c51eef271dcf522c65d71416037dbf439844201f
Julia Gottschalk <giulia.gottschalk@gmail.com>
2017-10-05 19:47:06 +0200

    done

---
+++ src/app/component/guest123/guest123.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'guest123',
+  templateUrl: './guest123.component.html',
+  styleUrls: ['./guest123.component.css']
+})
+export class Guest123Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
