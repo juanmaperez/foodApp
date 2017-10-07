760e917e5752d7b6ddb945b7674d7c7d4ab8e8c4
Julia Gottschalk <giulia.gottschalk@gmail.com>
2017-10-07 10:38:30 +0200

    done

--- src/app/component/join-community/join-community.component.ts
+++ src/app/component/join-community/join-community.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'join-community',
   templateUrl: './join-community.component.html',
-  styleUrls: ['./join-community.component.css']
+  styleUrls: ['./join-community.component.scss']
 })
 export class JoinCommunityComponent implements OnInit {
 
--- src/app/component/join/join.component.ts
+++ src/app/component/join/join.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-join',
   templateUrl: './join.component.html',
-  styleUrls: ['./join.component.css']
+  styleUrls: ['./join.component.scss']
 })
 export class JoinComponent implements OnInit {
 
