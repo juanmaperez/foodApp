ef566b0aa3f6bbf5221de48c8cea1aa2866650f0
Julia Gottschalk <giulia.gottschalk@gmail.com>
2017-10-08 10:42:05 +0200

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,51 @@
-import { ProfileComponent } from './component/profile/profile.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from "@angular/router";
-import { HttpModule } from '@angular/http';
-import { FileSelectDirective } from "ng2-file-upload";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProfileComponent }         from './component/profile/profile.component';
+import { BrowserModule }            from '@angular/platform-browser';
+import { NgModule }                 from '@angular/core';
+import { FormsModule }              from '@angular/forms';
+import { RouterModule, Routes }     from "@angular/router";
+import { HttpModule }               from '@angular/http';
+import { FileSelectDirective }      from "ng2-file-upload";
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './component/home/home.component';
-import { LoginComponent } from './component/login/login.component';
+import { AppComponent }             from './app.component';
+import { HomeComponent }            from './component/home/home.component';
+import { LoginComponent }           from './component/login/login.component';
 
-import { LayoutComponent } from './component/layout/layout.component';
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { FooterComponent } from './component/footer/footer.component';
-import { ApiService } from './services/api.service';
-import { SessionService } from './services/session.service';
+import { LayoutComponent }          from './component/layout/layout.component';
+import { NavbarComponent }          from './component/navbar/navbar.component';
+import { FooterComponent }          from './component/footer/footer.component';
+import { ApiService }               from './services/api.service';
+import { SessionService }           from './services/session.service';
 
-import { HeroComponent } from './component/hero/hero.component';
-import { HostVsguestComponent } from './component/host-vsguest/host-vsguest.component';
-import { JoinComponent } from './component/join/join.component';
-import { SignupComponent } from './component/signup/signup.component';
-import { Host123Component } from './component/host123/host123.component';
-import { Guest123Component } from './component/guest123/guest123.component';
-import { HostComponent } from './component/host/host.component';
-import { JoinCommunityComponent } from './component/join-community/join-community.component';
-import { HostsComponent } from './component/hosts/hosts.component';
-import { EventsComponent } from './component/events/events.component';
-import { EventComponent } from './component/event/event.component'; 
-
+import { HeroComponent }            from './component/hero/hero.component';
+import { HostVsguestComponent }     from './component/host-vsguest/host-vsguest.component';
+import { JoinComponent }            from './component/join/join.component';
+import { SignupComponent }          from './component/signup/signup.component';
+import { Host123Component }         from './component/host123/host123.component';
+import { Guest123Component }        from './component/guest123/guest123.component';
+import { HostComponent }            from './component/host/host.component';
+import { JoinCommunityComponent }   from './component/join-community/join-community.component';
+import { HostsComponent }           from './component/hosts/hosts.component';
+import { EventsComponent }          from './component/events/events.component';
+import { EventComponent }           from './component/event/event.component';
+import { CategoriesComponent }      from './component/categories/categories.component';
+import { CreateEventComponent }     from './component/create-event/create-event.component'; 
+import { EditProfileComponent }     from './component/edit-profile/edit-profile.component'; 
 
 // canActivate
 export const routes: Routes = [
-  { path: '',         component: HomeComponent   },
-  { path: 'login',    component: LoginComponent  }, 
-  { path: 'signup',   component: SignupComponent },
-  { path: 'profile',  component: ProfileComponent,  canActivate: [ SessionService ]  },
-  { path: 'events',   component: EventsComponent, 
-                      children: [ { path: ':id', component: EventComponent }] 
+  { path: '',               component: HomeComponent   },
+  { path: 'login',          component: LoginComponent  }, 
+  { path: 'signup',         component: SignupComponent },
+  { path: 'profile',        component: ProfileComponent,  canActivate: [ SessionService ]  },
+  { path: 'profile-edit',   component: EditProfileComponent,  canActivate: [ SessionService ]  },
+  { path: 'events',         component: EventsComponent, 
+         children: [ { path: ':id', component: EventComponent, canActivate: [ SessionService ] }] 
   },
-  { path: 'hosts',    component: HostsComponent,
-                      children: [ { path: ':id', component: HostComponent }] 
+  { path: 'create-event',   component: CreateEventComponent, canActivate: [ SessionService ] },
+  { path: 'categories',     component: CategoriesComponent },
+  { path: 'hosts',          component: HostsComponent,
+         children: [ { path: ':id', component: HostComponent, canActivate: [ SessionService ] }] 
   },  
   { path: '**', redirectTo: '' }
   
@@ -65,7 +70,17 @@ export const routes: Routes = [
     HostVsguestComponent,
     JoinComponent,
     ProfileComponent,
-    Guest123Component, Host123Component, SignupComponent, HostComponent, JoinCommunityComponent, HostsComponent, EventsComponent, EventComponent
+    Guest123Component, 
+    Host123Component, 
+    SignupComponent, 
+    HostComponent, 
+    JoinCommunityComponent, 
+    HostsComponent, 
+    EventsComponent, 
+    EventComponent, 
+    CategoriesComponent, 
+    CreateEventComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
