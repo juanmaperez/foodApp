5a78ee8cdfdf35eb8c2d39c06a6519aae8fe7a50
Julia Gottschalk <giulia.gottschalk@gmail.com>
2017-10-10 21:31:39 +0200

    google places api edit-profile

--- src/app/component/guest123/guest123.component.ts
+++ src/app/component/guest123/guest123.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations';
+import { NoopAnimationsModule }     from '@angular/platform-browser/animations';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'guest123',
   templateUrl: './guest123.component.html',
-  styleUrls: ['./guest123.component.css']
+  styleUrls: ['./guest123.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class Guest123Component implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  guestState:string =  "out"
+
+  toggle123Guest(){
+
+
+    this.guestState = this.guestState === 'out' ? 'in' : 'out';
+  
+
   }
 
 }
